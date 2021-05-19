5686229215ead45b7543c61e25a53fbd4317c7cd
Vikram Kamath <cvikramkamath@gmail.com>
2021-05-19 14:12:13 -0400

    Update XORModel.py

--- XORModel.py
+++ XORModel.py
@@ -1,14 +1,15 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 class XORNetwork(nn.Module):
 	def __init__(self):
+		# Initialize the different layers of the network
 		super(XORNetwork,self).__init__()
 		self.input_layer = nn.Linear(2,2)
 		self.hidden_layer = nn.Linear(2,1)
 
 	def forward(self,x):
+		# Define forward pass for the model
 		x1 = self.input_layer(x)
 		x1_ = torch.sigmoid(x1)
 		output = self.hidden_layer(x1_)
